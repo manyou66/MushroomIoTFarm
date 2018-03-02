473e52afc993e45c0d329f3f565d713d17cb2066
browniApp <wanpracha.n@rmutsv.ac.th>
2018-03-02 10:58:41 +0700

    Design UI Authen Success

--- app/src/main/java/wanpracha/rmutsv/ac/th/mushroomiotfarm/MainActivity.java
+++ app/src/main/java/wanpracha/rmutsv/ac/th/mushroomiotfarm/MainActivity.java
@@ -3,11 +3,20 @@ package wanpracha.rmutsv.ac.th.mushroomiotfarm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import wanpracha.rmutsv.ac.th.mushroomiotfarm.fragment.AuthenFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new AuthenFragment()).commit();
+
+        }//if
+    }//Main Method
+}//Main Class
